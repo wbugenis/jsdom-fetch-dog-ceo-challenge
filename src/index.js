01350e73c0c3a4ae2f36c5e89aec46a09b8ffe61
William Bugenis <wbugenis@gmail.com>
2021-02-27 21:06:09 -0500

    commit tree 98b50dbf87b22d2af23452cd2ce4fb0f17a12b90

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+// document.addEventListener("DOMContentLoaded", () => {
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach(msg => {
+            dogImgs = document.querySelector("div#dog-image-container")
+            const img = document.createElement('img')
+            img.src = msg
+            dogImgs.append(img)
+        })
+    })
+// })
